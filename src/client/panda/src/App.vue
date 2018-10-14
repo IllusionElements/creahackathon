ee76eec810efe92c7ce9cd85a5603d6694f7352d
Walter Cheng <walterpakwai@live.com>
2018-10-13 22:00:51 -0400

    create header and footer

--- src/client/panda/src/App.vue
+++ src/client/panda/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    
+    <header><a v-on:click="homepage()" style="color:#ffffff">-HOME-</a></header>
     <router-view/>
-    <!-- <img src="./assets/panda.jpg" height="300"> -->
+    <img src="./assets/pandabottom.png" width="100%"/>
   </div>
+  
 </template>
 
 <script>
@@ -11,10 +12,17 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './router'
 
 Vue.use(BootstrapVue);
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    homepage()
+    {
+      router.push({ name: "Page1"});
+    }
+  }
 }
 </script>
 
@@ -26,5 +34,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+header{
+  background: #222222;
+  text-align: left;
+  font-size: 35px;
+  color: #ffffff;
+  font-weight: bold;
 }
 </style>
